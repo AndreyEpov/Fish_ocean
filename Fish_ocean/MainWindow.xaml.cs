d54b27c3ef1dabe2043df0adbd6b3e61236b9e87
DanilUltrakill <yaremakadanil@gmail.com>
2019-10-11 14:20:48 +0700

    almostfirst

--- Fish_ocean/MainWindow.xaml.cs
+++ Fish_ocean/MainWindow.xaml.cs
@@ -24,16 +24,14 @@ namespace Fish_ocean
         {
             InitializeComponent();
         }
-      
-   
+
+        Random rnd = new Random();
+        int cordx, cordy;
 
         private void But_start_Click_1(object sender, RoutedEventArgs e)
         {
-            Random rnd = new Random();
-            int cordx,cordy;
-            cordx = (rnd.Next(1,18)*50);
-            cordy = (rnd.Next(1,12)*50);
-
+            fish_spawn();
+            tango_spawn();
         }
 
         private void But_start_Click(object sender, RoutedEventArgs e)
@@ -51,5 +49,72 @@ namespace Fish_ocean
         {
 
         }
+
+        private void fish_spawn(int fast,int stamina)
+        {
+            for (int i = fast; i>0; i--)
+            {
+                Rectangle fastfish = new Rectangle();
+
+                cordx = (rnd.Next(1, 18) * 50);
+                cordy = (rnd.Next(1, 12) * 50);
+
+                fastfish.Height = 25;
+                fastfish.Width = 50;
+                ImageBrush ib = new ImageBrush();
+                ib.AlignmentX = AlignmentX.Left;
+                ib.Stretch = Stretch.None;
+                ib.Viewbox = new Rect(0, 0, 50, 25);
+                ib.ViewboxUnits = BrushMappingMode.Absolute;
+                ib.ImageSource = new BitmapImage(new Uri(@"pack://application:,,,/текстурки/gg.gif", UriKind.Absolute));
+                fastfish.Fill = ib;
+                fastfish.Margin = new Thickness(cordx, cordy, 0, 0);
+                scene.Children.Add(fastfish);
+            }
+
+            for (int i = stamina; i > 0; i--)
+            {
+                Rectangle staminafish = new Rectangle();
+
+                cordx = (rnd.Next(1, 18) * 50);
+                cordy = (rnd.Next(1, 12) * 50);
+
+                staminafish.Height = 25;
+                staminafish.Width = 50;
+                ImageBrush ib = new ImageBrush();
+                ib.AlignmentX = AlignmentX.Left;
+                ib.Stretch = Stretch.None;
+                ib.Viewbox = new Rect(0, 0, 50, 25);
+                ib.ViewboxUnits = BrushMappingMode.Absolute;
+                ib.ImageSource = new BitmapImage(new Uri(@"pack://application:,,,/текстурки/gg.gif", UriKind.Absolute));
+                staminafish.Fill = ib;
+                staminafish.Margin = new Thickness(cordx, cordy, 0, 0);
+                scene.Children.Add(staminafish);
+            }
+        }
+
+        private void tango_spawn(int amountoftango)
+        {
+            for (int i = amountoftango; i > 0; i--)
+            {
+                Rectangle tango = new Rectangle();
+
+                cordx = (rnd.Next(1, 18) * 50);
+                cordy = (rnd.Next(1, 12) * 50);
+
+                tango.Height = 25;
+                tango.Width = 50;
+                ImageBrush ib = new ImageBrush();
+                ib.AlignmentX = AlignmentX.Left;
+                ib.Stretch = Stretch.None;
+                ib.Viewbox = new Rect(0, 0, 50, 25);
+                ib.ViewboxUnits = BrushMappingMode.Absolute;
+                ib.ImageSource = new BitmapImage(new Uri(@"pack://application:,,,/текстурки/gg.gif", UriKind.Absolute));
+                tango.Fill = ib;
+                tango.Margin = new Thickness(cordx, cordy, 0, 0);
+                scene.Children.Add(tango);
+            }
+        }
+
     }
 }
